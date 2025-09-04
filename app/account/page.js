b18afe9394611e9d4ca1bccb4345d844b97b9782
marwanf8 <marwanfathy86@gmail.com>
2025-09-04 05:07:22 +0300

    Build the structure of the website

--- app/account/page.js
+++ app/account/page.js
@@ -1,10 +1,13 @@
-
+export const metadata = {
+  title: "Guets area",
+};
 
 export default function Page() {
   return (
     <div>
-
-      <h1>Account page</h1>;
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Marwan
+      </h2>
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,34 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import { Josefin_Sans } from "next/font/google";
 
-export default function RootLayout({children}) {
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
+export const metadata = {
+  // title: 'The Wild Oasis',
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Mother of the World, surrounded by beatuiful mountains",
+};
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-        <Navigation />
-        </header>
-        <main>
-          {children}
-        </main>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
